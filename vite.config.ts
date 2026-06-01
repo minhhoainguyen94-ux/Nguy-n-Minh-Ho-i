0af5912859f49801de3646cb9584e7f16d3d10da
minhhoainguyen94-ux <minhhoainguyen94@gmail.com>
2026-06-01 11:12:27 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Trợ Lý Ai/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
